97ac5d45a65ecbc449a26f6cdea574a75d6170c5
André Sebastian <a.sebastian@posteo.de>
2019-04-05 12:23:50 +0200

    move data to data.js, create hover events and show corresponding text

---
+++ data.js
@@ -0,0 +1,9 @@
+const infotexts = {
+  Karrieweg: `Wie ein Fingerabdruck gleicht auch kein Karriereweg dem anderen. 
+              Wo und was Sie studieren, bei wem Sie Ihre Ausbildung machen, welchen 
+              Arbeitgeber Sie sich aussuchen, wie lange Sie bei einem Unternehmen 
+              bleiben und für welche Laufbahn Sie sich im Laufe der Zeit entscheiden: 
+              All das sind sehr individuelle Entscheidungen, die Ihren Karriereweg prägen. 
+              So sollte jeder den Weg einschlagen, der zu ihm selbst passt – zumindest in der Theorie.`,
+  Promotion: "Promotion"
+}
--- scripts.js
+++ scripts.js
@@ -1,10 +1,27 @@
 const mainButton = $("#main-button");
 const overlay = $("#overlay");
 const XButton = $("#overlay span");
+const infoText = $("#info-text")
+const infoBoxes = $(".info-box");
 
 function toggleOverlay () {
   overlay.toggle();
 }
 
+function showCorrespondingText (e) {
+  switch (this.id) {
+    case "ib1": infoText.text(infotexts.Karrieweg); break;
+    case "ib2": infoText.text("Punkt 2"); break;
+    default: console.log("empty"); 
+  }
+}
+
+function hideCorrespondingText() {
+  infoText.text("Bitte wählen Sie eine Kategorie oder Textsorte aus.")
+}
+
+// EVENT HANDLER
 mainButton.on( "click", toggleOverlay);
 XButton.on("click", toggleOverlay);
+infoBoxes.mouseenter(showCorrespondingText);
+infoBoxes.mouseleave(hideCorrespondingText)

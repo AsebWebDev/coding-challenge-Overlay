187d7d46df1e3456cf64bab83208781e43e6ce82
André Sebastian <a.sebastian@posteo.de>
2019-04-05 12:36:39 +0200

    add fake data

--- data.js
+++ data.js
@@ -5,5 +5,45 @@ const infotexts = {
               bleiben und für welche Laufbahn Sie sich im Laufe der Zeit entscheiden: 
               All das sind sehr individuelle Entscheidungen, die Ihren Karriereweg prägen. 
               So sollte jeder den Weg einschlagen, der zu ihm selbst passt – zumindest in der Theorie.`,
-  Promotion: "Promotion"
+
+  Promotion: `Die Promotion (lateinisch promotio ‚Beförderung') ist die Verleihung 
+              des akademischen Grades eines Doktors oder einer Doktorin in einem bestimmten 
+              Studienfach und in Form einer Promotionsurkunde.`,
+
+  Habilitation: `Die Habilitation ist die höchstrangige Hochschulprüfung in Deutschland, 
+                 Österreich, Frankreich, Liechtenstein, der Schweiz und einigen osteuropäischen 
+                 Ländern, mit der im Rahmen eines akademischen Prüfungsverfahrens die 
+                 Lehrbefähigung (lateinisch facultas docendi) in einem wissenschaftlichen 
+                 Fach festgestellt wird.`,
+  
+  Juniorproffesur: `Die Juniorprofessur ist eine Amtsbezeichnung für eine Stelle im Lehrkörper einer deutschen Hochschule.`,
+
+  Berufungssverfahren: `Die Berufung ist ein Rechtsmittel zur Überprüfung eines gerichtlichen 
+                        Urteils durch ein übergeordnetes Gericht. Die Berufung unterscheidet 
+                        sich hierbei von der Revision dadurch, dass das Ausgangsurteil nicht 
+                        nur in rechtlicher, sondern auch in tatsächlicher Hinsicht überprüft 
+                        wird, das Berufungsgericht also gegebenenfalls eine Beweisaufnahme 
+                        wiederholen und eigene Tatsachenfeststellungen treffen muss.`, 
+
+  Arbeitsplatz: `Als Arbeitsplatz (englisch work place) bezeichnet man in der Organisationslehre 
+                 eine räumlich eingegrenzte, mit Arbeitsmitteln ausgestattete Stelle in einer 
+                 Wirtschaftseinheit, an der eine Arbeitskraft ihre Arbeitsaufgaben verrichten kann.`,
+
+  FamiliePartnerschaftAlltag: `Eine enge Partnerschaft und Ehe hat auch heute, trotz einem vorübergehenden 
+                              Einbruch der Heiratshäufigkeit im letzten Jahr, ihre Bedeutung nicht eingebüsst. 
+                              Rund 90 Prozent der Bevölkerung heiratet mindestens einmal im Verlauf des Lebens. 
+                              Obwohl sich Beziehungskrisen und Scheidungen häufen, wünschen sich die meisten Menschen 
+                              noch immer eine stabile Partnerschaft.`,
+
+  Mobilität: `Menschen – Güter – Daten: Alles ist mobil oder soll mobil sein. Google findet 500 Millionen 
+              Treffer für das Wort „mobil“, für Mobilität immerhin noch fast 15 Millionen. Das ist – 
+              freundlich ausgedrückt – unübersichtlich. „Mobilitätsdienstleister“, „Datenmobilität“, 
+              „Elektromobilität“, „mobile Alte“, „Jugendmobil“ und und und.`,
+
+  BlickAusDemAusland: `Der starke Franken lässt die Ladenkassen ennet der Grenze klingeln. Einen neue Umfrage 
+                      zeigt: 70 Prozent der Schweizer kaufen im Ausland ein, vor allem Lebensmittel und Kleider.`,
+
+  Allgemein: `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut 
+              labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo 
+              dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. `
 }
--- scripts.js
+++ scripts.js
@@ -11,8 +11,16 @@ function toggleOverlay () {
 function showCorrespondingText (e) {
   switch (this.id) {
     case "ib1": infoText.text(infotexts.Karrieweg); break;
-    case "ib2": infoText.text("Punkt 2"); break;
-    default: console.log("empty"); 
+    case "ib2": infoText.text(infotexts.Promotion); break;
+    case "ib3": infoText.text(infotexts.Habilitation); break;
+    case "ib4": infoText.text(infotexts.Juniorproffesur); break;
+    case "ib5": infoText.text(infotexts.Berufungssverfahren); break;
+    case "ib6": infoText.text(infotexts.Arbeitsplatz); break;
+    case "ib7": infoText.text(infotexts.FamiliePartnerschaftAlltag); break;
+    case "ib8": infoText.text(infotexts.Mobilität); break;
+    case "ib9": infoText.text(infotexts.BlickAusDemAusland); break;
+    case "ib10": infoText.text(infotexts.Allgemein); break;
+    default: console.log("Bitte wählen Sie eine Kategorie oder Textsorte aus."); 
   }
 }
 
